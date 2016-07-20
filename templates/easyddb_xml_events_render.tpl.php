404ab0a7c2104fee9b8a841e4883f4cc60392441
MM <mm@inlead.dk>
2016-07-20 16:40:30 +0300

    EASYOPAC-228 - Create module.

---
+++ templates/easyddb_xml_events_render.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Template file for viewer container.
+ *
+ * Variables:
+ *
+ */
+?>
+
+<?php print '<?xml version="1.0"?>'; ?>
+<document>
+  <libraryName><?php print $library->title; ?></libraryName>
+</document>
